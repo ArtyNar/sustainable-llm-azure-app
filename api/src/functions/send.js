1da72d9aeeb52dd6e33b05fbea693c523116b69a
ArtyNar <arty.nar00@gmail.com>
2025-09-18 16:43:33 -0600

    Fix functions, add index.js

--- api/src/functions/send.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.http('send', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        return { body: JSON.stringify({ "text": `Hello, from the API!` }) };
-    }
-});
