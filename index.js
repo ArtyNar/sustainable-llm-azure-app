161343371fb4bfec50d0737cf19ea83013311f7c
ArtyNar <arty.nar00@gmail.com>
2025-11-18 13:50:55 -0700

    Add chart

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Auto-fetch on load
   fetchCarbonIntesnityData();
   fetchPrompts();
+  plotChart();
 
   // Send button click handler
   document.querySelector('#send').addEventListener('click', handleSend);
@@ -10,11 +11,32 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#schedule').addEventListener('click', handleSchedule);
 });
 
+async function plotChart() {
+  const ctx = document.getElementById('myChart').getContext('2d');
+          const myChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                  datasets: [{
+                      label: 'Carbon Intensity (gCO2eq/kWh)',
+                      data: [6, 10, 3, 5, 2, 3],
+                      backgroundColor: [
+                          'rgba(255, 206, 86, 0.2)',
+                      ],
+                      borderColor: [
+                          'rgba(255, 206, 86, 1)',
+                      ],
+                      borderWidth: 1
+                  }]
+              }
+          });
+        }
+
 async function fetchCarbonIntesnityData() {
   try {
     const res = await fetch('/api/carbon-intensity');
     const data = await res.json(); 
-    
+    console.log("EM current CI:", data)
     document.querySelector('#response2').innerHTML = "Current carbon intensity: <b>" + data.carbonIntensity +" gCO2eq/kWh</b><br>Grid: " + data.zone + " " + data.zone_name;
   } catch (err) {
       document.querySelector('#response2').textContent = 'Error: ' + err.message;
